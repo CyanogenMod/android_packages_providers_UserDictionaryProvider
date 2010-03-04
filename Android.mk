0460fc17bc6b3ad801b78b1c9ee6b7f7a3ff2429
Ying Wang <wangying@google.com>
2010-03-03 17:36:45 -0800

    Enable Proguard on UserDictionaryProvider.

--- Android.mk
+++ Android.mk
@@ -10,6 +10,6 @@ LOCAL_JAVA_LIBRARIES := ext
 LOCAL_PACKAGE_NAME := UserDictionaryProvider
 LOCAL_CERTIFICATE := shared
 
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
 
 include $(BUILD_PACKAGE)
